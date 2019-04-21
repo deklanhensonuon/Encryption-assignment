282f29f5052eb7806ade13a23f795b0ce3320119
deklanhensonuon <48206735+deklanhensonuon@users.noreply.github.com>
2019-04-21 22:26:49 +1000

    Create rotation.c

---
+++ rotation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+nt main() {
+ char sentence[] = "hi how are you";
+    
+    char array[26]="abcdefghijklmnopqrstuvwxyz";
+    
+    int i = 0; 
+    
+    while (i <15)
+     {
+         if(sentence[i]==' '){
+             
+         }
+         else if(sentence[i]=='{'){
+         
+         }
+          
+        else 
+            {
+             sentence[i] = sentence[i] + 2;
+         }
+         
+         i++; 
+     }
+    
+    
